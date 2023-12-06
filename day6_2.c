028f1dd50fc976a7a486d5531423e7213639a8cf
Gaspard Quentin <gaspard.quentin1905@gmail.com>
2023-12-06 20:28:57 +0100

    add timer to check execution time

--- day6_2.c
+++ day6_2.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
-
-
+#include <time.h>
 
 long day6_partial(long time, long distance) {
     long res = 0;
@@ -11,14 +10,13 @@ long day6_partial(long time, long distance) {
     return res;
 }
 
-
-
-
 int main(int argc, char **argv) {
-
+    clock_t t1, t2;
     long res;
+    t1 = clock();
     res = day6_partial(53897698, 313109012141201);
+    t2 = clock();
     printf("%ld \n", res);
-
+    printf("%f \n", (double)(t2-t1)/(double)CLOCKS_PER_SEC);
     return 0;
 }
